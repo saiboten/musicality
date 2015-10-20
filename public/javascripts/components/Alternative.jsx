72a4a5f8d4bfad664f35946245e9d6d82570ed7d
saiboten <saiboten@gmail.com>
2015-10-20 21:29:38 +0200

    Added a cool new library to visualize things!

--- public/javascripts/components/Alternative.jsx
+++ public/javascripts/components/Alternative.jsx
@@ -1,6 +1,11 @@
 var React = require('react');
+var debug = require('debug')('Alternative');
 
 var Alternative = React.createClass({
+
+    isCreated: false,
+
+    wavesurfer: undefined,
 
     getInitialState() {
         return {
@@ -11,8 +16,10 @@ var Alternative = React.createClass({
     },
 
     play() {
-        if(this.state.selected) {
-            React.findDOMNode(this.refs.play).play();
+        if(this.state.selected && this.isCreated) {
+            //React.findDOMNode(this.refs.play).play();
+            this.wavesurfer.seekTo(0);
+            this.wavesurfer.play();
         }
     },
 
@@ -23,22 +30,52 @@ var Alternative = React.createClass({
     },
 
     playPause() {
-        if(this.state.playing) {
-            React.findDOMNode(this.refs.play).pause();
+        if(this.isCreated) {
+            if(this.state.playing) {
+                //React.findDOMNode(this.refs.play).pause();
+                this.wavesurfer.pause();
+            }
+            else {
+                //React.findDOMNode(this.refs.play).play();
+                this.wavesurfer.play();
+            }
+
+            this.setState( {
+                playing: !this.state.playing,
+                playtext: this.state.playing ? "images/glyphicons-174-play.png":"images/glyphicons-175-pause.png"
+            });
+        }
+    },
+
+    setup() {
+        if(this.isCreated) {
+            debug("Already created");
         }
         else {
-            React.findDOMNode(this.refs.play).play();
-        }
+            var wavesurfer = Object.create(WaveSurfer);
 
-        this.setState( {
-            playing: !this.state.playing,
-            playtext: this.state.playing ? "images/glyphicons-174-play.png":"images/glyphicons-175-pause.png"
-        });
+            wavesurfer.init({
+                container: React.findDOMNode(this.refs.alternativeAudio),
+                waveColor: 'violet',
+                backend: 'MediaElement'
+            });
+
+            wavesurfer.load(this.props.alternative.href);
+            var that = this;
+            wavesurfer.on('ready', function () {
+                debug('Song is loaded - and is ready for action!');
+                that.isCreated = true;
+                that.wavesurfer = wavesurfer;
+            });
+
+        }
     },
 
     render() {
         return (
             <li className="alternative">
+                <div ref="alternativeAudio"></div>
+                <button onClick={this.setup}>Last låt</button>
                 <audio preload="none" ref="play" src={this.props.alternative.href}>Hallo</audio>
                 {this.props.alternative.name}
                 <img src={this.state.playtext} onClick={this.playPause} />
